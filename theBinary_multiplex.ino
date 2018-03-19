e0161bfbcf38d55d02a9f47288912c1053c5248f
Lucas Drozdz <warior1112@gmail.com>
2018-03-19 22:19:34 +0100

    1.Support for RTC and more intuitive multiplexing

--- theBinary_multiplex.ino
+++ theBinary_multiplex.ino
@@ -1,5 +1,15 @@
+#include<Wire.h>
+#include"RTClib.h" // by NeiroN
+
+
 #define hours_s 2
 #define minutes_s 3
+
+int h;
+int m;
+const int refr_rate;
+
+DS1302 rtc(CE, CLK, IO);
 
 void setDataRail(int n){
   PORTB=n;
@@ -11,17 +21,35 @@ void setActive(int segment){
   digitalWrite(segment, LOW);
 }
 
-void multiplex(int h, int m, int t, int n){
+void multiplex(int h, int m, int t){
 
-  for(int i=0;i<n;i++){
-    setActive(hours_s);
-    setDataRail(h);
-    delay(t);
-    setActive(minutes_s);
-    setDataRail(m);
-    delay(t);
-  }
+    unsigned long t0 = millis();
+    while(millis()-t0<t*1000){
+        setActive(hours_s);
+        setDataRail(h);
+        delay(500/refr_rate);
+        setActive(minutes_s);
+        setDataRail(m);
+        delay(500/refr_rate);
+    }
 
 }
 
+void setup(){
+    
+    Wire.begin();
+    rtc.begin();
+  
+    if (! rtc.isrunning()) {
+        rtc.adjust(DateTime(__DATE__, __TIME__)); //get the damn thing up and running
+    }
+    
+}
+
+void loop(){
+    DateTime data = rtc.now;
+    h = data.hour();
+    m = data.minute();
+    multiplex(h, m, 60); //we only need to recheck after 60 seconds
+}
 
